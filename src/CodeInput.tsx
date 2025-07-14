0c99b02d2810ce599505cf85b30055f3f554ac54
Jacob511-hub <jacob511@outlook.com>
2025-07-14 14:30:23 -0230

    Added images to buttons

--- src/CodeInput.tsx
+++ src/CodeInput.tsx
@@ -2,7 +2,16 @@ import React from "react";
 
 const CodeInput: React.FC = () => {
     return (
-        <div className="code-input"/>
+        <div className="code-input">
+            <div
+                style={{
+                    backgroundImage: "url(/images/CodeRed.png)",
+                    width: '100%',
+                    height: '100%',
+                    backgroundSize: 'cover'
+                }}>
+            </div>
+        </div>
     )
 };
 
--- src/ConfirmButton.tsx
+++ src/ConfirmButton.tsx
@@ -2,7 +2,16 @@ import React from "react";
 
 const ConfirmButton: React.FC = () => {
     return (
-        <div className="code-input"/>
+        <div className="code-input">
+            <div
+                style={{
+                    backgroundImage: "url(/images/Confirm.png)",
+                    width: '100%',
+                    height: '100%',
+                    backgroundSize: 'cover'
+                }}>
+            </div>
+        </div>
     )
 };
 
