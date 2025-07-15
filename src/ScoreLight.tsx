042bc567da9f5db87be20c6691bf7f0d01d196cc
Jacob511-hub <jacob511@outlook.com>
2025-07-15 13:20:11 -0230

    Added background + score light colours

--- src/ScoreLight.tsx
+++ src/ScoreLight.tsx
@@ -2,7 +2,16 @@ import React from "react";
 
 const ScoreLight: React.FC = () => {
     return (
-        <div className="score-light"/>
+        <div className="score-light">
+            <div
+                style={{
+                    backgroundImage: "url(/images/ScoreBlack.png)",
+                    width: '100%',
+                    height: '100%',
+                    backgroundSize: 'cover'
+                }}>
+            </div>
+        </div>
     )
 };
 
