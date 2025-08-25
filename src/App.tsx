569033e32eb5af7ecbf28acc4a121380536b0a95
Jacob511-hub <jacob511@outlook.com>
2025-08-25 15:41:14 -0230

    Added space for score guide

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Board from './Board';
+import Guide from './Guide';
 import { AnswerArrayProvider } from './AnswerArrayContext';
 
 const App: React.FC = () => {
   return (
     <AnswerArrayProvider>
       <div className="App">
+        <Guide />
         <Board />
       </div>
     </AnswerArrayProvider>
---
+++ src/Guide.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Guide: React.FC = () => {
+    return (
+        <div className="guide">
+        </div>
+    );
+};
+
+export default Guide;
