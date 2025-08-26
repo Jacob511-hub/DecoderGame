9b8822cc35c5c69d1f5ba9706dd00143225e1f7b
Jacob511-hub <jacob511@outlook.com>
2025-08-26 14:52:59 -0230

    Created score guide

--- src/Guide.tsx
+++ src/Guide.tsx
@@ -3,6 +3,24 @@ import React from "react";
 const Guide: React.FC = () => {
     return (
         <div className="guide">
+            <div className="score-guide">
+                <img
+                    src="/images/ScoreGreen.png"
+                    style={{ height: "80%", marginRight: "10px" }}
+                />
+                <span style={{ color: "white", fontSize: "1.4em" }}>
+                    Correct color in correct position
+                </span>
+            </div>
+            <div className="score-guide">
+                <img
+                    src="/images/ScoreYellow.png"
+                    style={{ height: "80%", marginRight: "10px" }}
+                />
+                <span style={{ color: "white", fontSize: "1.4em" }}>
+                    Correct color in wrong position
+                </span>
+            </div>
         </div>
     );
 };
